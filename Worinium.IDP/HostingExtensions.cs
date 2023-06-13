b7aa38c4677263736587dc4e32c924623b478abc
Hearty Alapher <alapherwori8g5@gmail.com>
2023-06-14 00:09:51 +0100

    Modified HostingExtensions class

--- Worinium.IDP/HostingExtensions.cs
+++ Worinium.IDP/HostingExtensions.cs
@@ -20,25 +20,25 @@ internal static class HostingExtensions
 
         return builder.Build();
     }
-    
+
     public static WebApplication ConfigurePipeline(this WebApplication app)
-    { 
+    {
         app.UseSerilogRequestLogging();
-    
+
         if (app.Environment.IsDevelopment())
         {
             app.UseDeveloperExceptionPage();
         }
 
         // uncomment if you want to add a UI
-        //app.UseStaticFiles();
-        //app.UseRouting();
-            
+        app.UseStaticFiles();
+        app.UseRouting();
+
         app.UseIdentityServer();
 
         // uncomment if you want to add a UI
-        //app.UseAuthorization();
-        //app.MapRazorPages().RequireAuthorization();
+        app.UseAuthorization();
+        app.MapRazorPages().RequireAuthorization();
 
         return app;
     }
